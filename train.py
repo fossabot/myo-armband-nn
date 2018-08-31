7886426573454b157e78fad7ef10291cc4375321
kyr7 <kirillskiy@gmail.com>
2018-09-01 00:11:53 +0300

    model export

--- train.py
+++ train.py
@@ -1,9 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
 import numpy as np
 import tensorflow as tf
+
 from time import time
 from include.data import get_data_set
 from include.model import model
+from tensorflow.python.saved_model.builder_impl import SavedModelBuilder
 
+tf.app.flags.DEFINE_integer('model_version', 1, 'version number of the model.')
+FLAGS = tf.app.flags.FLAGS
 
 train_x, train_y = get_data_set()
 
@@ -28,6 +38,7 @@ init = tf.global_variables_initializer()
 merged = tf.summary.merge_all()
 saver = tf.train.Saver()
 sess = tf.Session()
+serialized_tf_example = tf.placeholder(tf.string, name='myo-nn')
 train_writer = tf.summary.FileWriter(_SAVE_PATH, sess.graph)
 
 
@@ -65,5 +76,55 @@ def train(num_iterations = 1000):
 
 train(7500)
 
+tensor_info_x = tf.saved_model.utils.build_tensor_info(x)
+tensor_info_y = tf.saved_model.utils.build_tensor_info(y)
+
+table = tf.contrib.lookup.index_to_string_table_from_tensor(tf.constant([str(i) for i in range(_CLASS_SIZE)]))
+values, indices = tf.nn.top_k(y, _CLASS_SIZE)
+prediction_classes = table.lookup(tf.to_int64(indices))
+
+
+classification_inputs = tf.saved_model.utils.build_tensor_info(serialized_tf_example)
+classification_outputs_classes = tf.saved_model.utils.build_tensor_info(prediction_classes)
+classification_outputs_scores = tf.saved_model.utils.build_tensor_info(values)
+
+classification_signature = (
+  tf.saved_model.signature_def_utils.build_signature_def(
+      inputs={
+          tf.saved_model.signature_constants.CLASSIFY_INPUTS:
+              classification_inputs
+      },
+      outputs={
+          tf.saved_model.signature_constants.CLASSIFY_OUTPUT_CLASSES:
+              classification_outputs_classes,
+          tf.saved_model.signature_constants.CLASSIFY_OUTPUT_SCORES:
+              classification_outputs_scores
+      },
+      method_name=tf.saved_model.signature_constants.CLASSIFY_METHOD_NAME))
+
+prediction_signature = (
+  tf.saved_model.signature_def_utils.build_signature_def(
+      inputs={'images': tensor_info_x},
+      outputs={'scores': tensor_info_y},
+      method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME))
+
+export_path_base = "/script-1/data"
+export_path = os.path.join(
+      tf.compat.as_bytes(export_path_base),
+      tf.compat.as_bytes(str(FLAGS.model_version)))
+print("Exporting trained model to", export_path)
+builder = tf.saved_model.builder.SavedModelBuilder(export_path)
+
+
+builder.add_meta_graph_and_variables(
+  sess, [tf.saved_model.tag_constants.SERVING],
+  signature_def_map={
+      'predict':
+          prediction_signature,
+      tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
+          classification_signature,
+  },
+  main_op=tf.tables_initializer(),
+  strip_default_attrs=True)
 
 sess.close()
